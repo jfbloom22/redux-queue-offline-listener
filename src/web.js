6844a9a132d954082de8738ab247d944b2095f28
Jonathan Flower <jon@simplejoymedia.com>
2018-12-11 14:52:13 -0600

    update web for React 16

--- src/web.js
+++ src/web.js
@@ -1,33 +1,49 @@
-import React from 'react'
-import { ONLINE, OFFLINE } from 'redux-queue-offline'
+'use strict';
 
-const NetworkListener = Provider => React.createClass({
-  displayName: 'NetworkListener',
-  componentDidMount () {
-    if (window && window.addEventListener) {
-      window.addEventListener('online', this._onlineListener)
-      window.addEventListener('offline', this._offlineListener)
-    }
-  },
-  componentWillUnmount () {
-    if (window && window.removeEventListener) {
-      window.removeEventListener('online', this._onlineListener)
-      window.removeEventListener('offline', this._offlineListener)
-    }
-  },
-  render () {
-    return <Provider {...this.props} />
-  },
-  _onlineListener () {
-    this.props.store.dispatch({
-      type: ONLINE
-    })
-  },
-  _offlineListener () {
-    this.props.store.dispatch({
-      type: OFFLINE
-    })
-  }
-})
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
 
-export default NetworkListener
+var _react = require('react');
+
+var _react2 = _interopRequireDefault(_react);
+
+var _reduxQueueOffline = require('redux-queue-offline');
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+var NetworkListener = (Provider) => {
+  return class NetworkListenerComponent extends _react.Component {
+    constructor(props) {
+    super(props);
+    }
+    // static displayName = 'NetworkListener'
+    componentDidMount() {
+      if (window && window.addEventListener) {
+        window.addEventListener('online', this._onlineListener);
+        window.addEventListener('offline', this._offlineListener);
+      }
+    }
+    componentWillUnmount() {
+      if (window && window.removeEventListener) {
+        window.removeEventListener('online', this._onlineListener);
+        window.removeEventListener('offline', this._offlineListener);
+      }
+    }
+    render() {
+      return _react2.default.createElement(Provider, this.props);
+    }
+    _onlineListener() {
+      this.props.store.dispatch({
+        type: _reduxQueueOffline.ONLINE
+      });
+    }
+    _offlineListener() {
+      this.props.store.dispatch({
+        type: _reduxQueueOffline.OFFLINE
+      });
+    }
+  };
+};
+
+exports.default = NetworkListener;
